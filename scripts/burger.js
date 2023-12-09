f090ad11a8a2592bc17bb87e687e24c9451c3621
hawkstan <zoe.ranzy@gmail.com>
2023-12-09 01:28:16 +0100

    burger start

---
+++ scripts/burger.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const [open, setOpen] = useState(false);
+
+    setOpen(!open);
+}
+
+// got a burger menu working in react, you can do it on pure js
+// do the important things first tho
